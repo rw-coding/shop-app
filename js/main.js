d107745fdae2717f814638790bda6d8656009a4a
Richard Alan Walker <richard@Richards-MacBook-Pro.local>
2020-03-29 15:21:34 -0500

    About page and contact pages remaining

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 let nashville = {lat: 36.174465, lng: -86.767960};
 let mapEmbed = new google.maps.Map(document.getElementById("the-map"), {zoom: 4, center: nashville});
 
-/*
 function open() {
-    $("#hamburger-overlay").animate({width: "100%"}, 10000);
+    $(".hamburger-overlay").css("width","100%");
 }
 function close() {
-    $("#hamburger-overlay").animate({width: "0%"}, 10000);
+    $(".hamburger-overlay").css("width","0");
 }
-*/
